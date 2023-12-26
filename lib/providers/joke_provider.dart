774e96d8bb16364232e1031841f21fd8edd7df8b
sankalpchavada <129574480+sankalpchavada@users.noreply.github.com>
2023-12-26 12:48:05 +0530

    Update joke_provider.dart
    just removed the comments of this file

--- lib/providers/joke_provider.dart
+++ lib/providers/joke_provider.dart
@@ -16,28 +16,3 @@ class JokesProvider extends ChangeNotifier {
     }
   }
 }
-
-// class JokesList extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     final jokesProvider = Provider.of<JokesProvider>(context);
-//
-//     return Column(
-//       children: [
-//         ElevatedButton(
-//           onPressed: () => jokesProvider.fetchJokes(),
-//           child: Text('Fetch Chuck Norris Joke'),
-//         ),
-//         if (jokesProvider.jokes.isNotEmpty)
-//           ListTile(
-//             title: Text(jokesProvider.jokes[0].value),
-//             subtitle: Text(
-//                 'Category: ${jokesProvider.jokes[0].categories.join(', ')}'),
-//             leading: CircleAvatar(
-//               backgroundImage: NetworkImage(jokesProvider.jokes[0].iconUrl),
-//             ),
-//           ),
-//       ],
-//     );
-//   }
-// }
